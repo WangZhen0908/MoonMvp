ae52f4795a0175ac3bb95bd260a248693eac8d08
wangzhen <wangzhen@bestpay.com.cn>
2020-07-13 12:24:46 +0800

    测试

--- app/src/main/java/com/moon/mvp/demo/main/fragment/MainFragment.java
+++ app/src/main/java/com/moon/mvp/demo/main/fragment/MainFragment.java
@@ -1,11 +1,17 @@
 package com.moon.mvp.demo.main.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.alipay.android.phone.scancode.export.ScanCallback;
+import com.alipay.android.phone.scancode.export.ScanRequest;
+import com.alipay.android.phone.scancode.export.adapter.MPScan;
 import com.moon.common.base.fragment.BaseFragment;
+import com.moon.library.utils.ToastUtils;
 import com.moon.mvp.demo.R;
 import com.moon.mvp.demo.main.contract.MainContract;
 import com.moon.mvp.demo.main.presenter.MainPresenter;
@@ -26,6 +32,12 @@ public class MainFragment extends BaseFragment<MainContract.Presenter> implement
         super.initViews(view, args);
         mHotWordsCountTv = view.findViewById(R.id.hot_words_count_tv);
         mHotWordsCountTv.setText("上次：" + mPresenter.getHotWords());
+        mHotWordsCountTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scan();
+            }
+        });
     }
 
     @Override
@@ -47,5 +59,32 @@ public class MainFragment extends BaseFragment<MainContract.Presenter> implement
     @Override
     public void showHotWordsCount(int count) {
         mHotWordsCountTv.setText("最新：" + count);
+    }
+
+    private void scan() {
+        ScanRequest scanRequest = new ScanRequest();
+        scanRequest.setScanType(ScanRequest.ScanType.QRCODE);
+        MPScan.startMPaasScanActivity(getActivity(), scanRequest, new ScanCallback() {
+            @Override
+            public void onScanResult(final boolean isProcessed, final Intent result) {
+                if (!isProcessed) {
+                    // 扫码界面点击物理返回键或左上角返回键
+                    return;
+                }
+                // 注意：本回调是在子线程中执行
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (result == null || result.getData() == null) {
+                            // 扫码失败
+                            return;
+                        }
+                        // 扫码成功
+                        String url = result.getData().toString();
+                        Toast.makeText(getActivity(), url, Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
     }
 }
